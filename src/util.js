3f01c75536332f6eef1757fe417dc221d1051c19
7thSunday <1064578256@qq.com>
2021-06-12 11:53:23 +0800

    add lemalin_p

--- src/util.js
+++ src/util.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const url = '/ARC/data/index04012021.json';
+const url = '/ARC/data/index05042021.json';
 export default {
   fetchArticleList(callback) {
     axios.get(url).then((res) => {
